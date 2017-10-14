da59afab6f69d36f051e5e4237f6808e5cbd9153
Junmax <player454@mail.ru>
2017-10-14 13:34:10 +0500

    Simple validation was added

--- gulpfile.js
+++ gulpfile.js
@@ -52,10 +52,12 @@ gulp.task('styles:compile', function () {
 /* ------------ js ------------- */
 gulp.task('js', function () {
   return gulp.src([
-      'source/js/form.js',
-      'source/js/navigation.js',
-      'source/js/main.js'
-    ])
+    'source/js/init.js',
+    'source/js/validation.js',
+    'source/js/form.js',
+    'source/js/navigation.js',
+    'source/js/main.js'
+  ])
     .pipe(sourcemaps.init())
     .pipe(concat('main.min.js'))
     .pipe(uglify())
--- source/js/form.js
+++ source/js/form.js
@@ -8,16 +8,56 @@
     closeButton.removeEventListener('click', onClose);
   }
 
+  function closeOnEsc(e) {
+    if (e.keyCode === 27) {
+      me.close();
+      window.removeEventListener('keyup', closeOnEsc);
+    }
+  }
+
   me.open = function () {
     form.classList.remove('is-hidden');
 
     closeButton = document.querySelector('.form__close-button');
     closeButton.addEventListener('click', onClose);
+    window.addEventListener('keyup', closeOnEsc);
   };
 
   me.close = function () {
     form.classList.add('is-hidden');
   };
 
-  window.form = me;
+  me.isValid = function () {
+    var requiredFields = document.querySelectorAll('[data-valid="required"]');
+    var emailValue = document.querySelector('[data-email]').value;
+    var numberValue = document.querySelector('[data-number]').value;
+
+    if (!me.isAllCompleted(requiredFields)) {
+      console.log('Заполните, пожалуйста все необходимые поля');
+      return false;
+    } else if (!contacts.validation.isEmail(emailValue)) {
+      console.log('Введите корректный Email');
+      return false;
+    } else if (!contacts.validation.isNumber(numberValue)) {
+      console.log('Неверный номер');
+      return false;
+    }
+
+    return true;
+  };
+
+  me.isAllCompleted = function (data) {
+    var result = true;
+
+    for (var i = 0; i < data.length; i++) {
+      if (!contacts.validation.isNotEmpty(data[i].value)) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  };
+
+  contacts.form = me;
 }());
